ff6dea88ccf028ff3f5dc6fb55417d124f0576ac
joe ellsworth <joexdobs@gmail.com>
2018-11-20 18:14:46 -0500

    testing

---
+++ hello/hello.go
@@ -0,0 +1,20 @@
+/* Launch a single named VM.  Then pull it's IP from lxc info and append it as a
+property to the end of a properties file */
+
+package  "main"
+
+import (
+	"fmt"
+)
+
+// see: https://gobyexample.com/command-line-flags
+
+//import "codesite.tld/authorName/Go-PublishingExample/epub"
+
+//var dir = flag.String("dir", ".", "Directory to publish")
+
+func main() {
+	fmt.Println("in jlxc-start-register-single")
+	//flag.Parse()
+	//epub.Publish(*dir)
+}
